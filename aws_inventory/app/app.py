f8c9123cd5006206eade889d39d81f196178933b
Yannis Folias <yannis.folias@gmail.com>
2019-03-23 13:33:25 +0000

    Initialising project

--- aws_inventory/app/app.py
+++ aws_inventory/app/app.py
@@ -1,2 +1,10 @@
-#!/usr/bin/env python3
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>OC Solutions</h1>\n<h3>AWS Inventory</h3>"
+@app.route("/ec2")
+def ec2():
+    return "<h1>OC Solutions</h1>\n<h3>AWS Inventory</h3>"
